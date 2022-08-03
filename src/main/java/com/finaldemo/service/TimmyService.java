1a41a32b17943087857843f190f49dbc17f36cd7
timmy <timmy860930@gmail.com>
2022-08-03 11:07:59 +0800

    testing

--- src/main/java/com/finaldemo/service/TimmyService.java
+++ src/main/java/com/finaldemo/service/TimmyService.java
@@ -1,10 +1,17 @@
 package com.finaldemo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.finaldemo.model.UsersRepository;
 
 @Service
 @Transactional
 public class TimmyService {
-
+	
+	@Autowired
+	private UsersRepository uDao;
+	
+	
 }
