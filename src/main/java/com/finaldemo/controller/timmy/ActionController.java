3e9149ea563e5348903086bef50a7553107796a2
timmy <timmy860930@gmail.com>
2022-08-03 11:16:28 +0800

    資料庫登入帳號密碼統一： sa 、1234

--- src/main/java/com/finaldemo/controller/timmy/ActionController.java
+++ src/main/java/com/finaldemo/controller/timmy/ActionController.java
@@ -1,9 +1,13 @@
 package com.finaldemo.controller.timmy;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ActionController {
 	
-	
+	@GetMapping("accountsetting.controller")
+	public String testgivingSession() {
+		return "timmy/normalmember";
+	}
 }
