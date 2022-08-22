2ac82edad584d208d9db06f6bd3729b41f1d577c
DESKTOP-F9UVJQG\Student <Git>
2022-08-22 23:54:08 +0800

    可新增複數商品進購物車   購物車新增刪除功能 會員可以查到自己的購物車

--- src/main/java/com/finaldemo/model/ShoppingCarRepository.java
+++ src/main/java/com/finaldemo/model/ShoppingCarRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,4 +16,10 @@ public interface ShoppingCarRepository extends JpaRepository<ShoppingCar, Intege
 
 	@Query(value = "SELECT * FROM ShoppingCar S INNER JOIN Products P on S.fk_product_Id = P.productId  WHERE S.fk_user_id= ?1" ,nativeQuery = true)
 	public List<ShoppingCar> ShoppingCarProducts(@Param("userId") String fk_user_id);	
+	
+	//刪除
+	@Transactional
+	@Modifying
+	@Query(value = "DELETE FROM ShoppingCar WHERE shoppingCarId = ?1" ,nativeQuery = true)
+	public void deleteShoppingCarProducts(@Param("shoppingCarId") Integer shoppingCarId);	
 }
